24af961736511129eb788549e1bc5eda68632a75
Richard Ngo-Lam <rngolam@gmail.com>
2022-10-30 06:47:13 -0700

    Create ID parser helper

---
+++ backend/utils/getIdFromBodyParam.js
@@ -0,0 +1,12 @@
+// Asserts the ID provided in a request body is a valid numerical string and converts it to a number
+// Adapted from https://github.com/sequelize/express-example/blob/master/express-main-example/express/helpers.js
+
+function getIdFromBodyParam(req) {
+  const id = req.body.id;
+  if (/^\d+$/.test(id)) {
+    return Number.parseInt(id, 10);
+  }
+  throw new TypeError(`Invalid ID: "${id}"`);
+}
+
+export default getIdFromBodyParam;

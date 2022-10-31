ab2819386ead05dd8db2dbe04a1a776ff7649fcf
Kevin <kevinsekuj@gmail.com>
2022-10-30 18:00:20 -0700

    revert axios post to get request

--- frontend/src/common/service.js
+++ frontend/src/common/service.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { JOBS_ENDPOINT_URL } from "common/constants";
 
 export default async function getRowData(email) {
-  const response = await axios.post(JOBS_ENDPOINT_URL, {
-    userEmail: email,
+  const response = await axios.get(JOBS_ENDPOINT_URL, {
+    params: { userEmail: email },
   });
   if (response.status === 200) return response.data;
 }

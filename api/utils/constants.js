9d0bc420db474c014c3fc94767e5fc67e1cf7538
Richard Ngo-Lam <rngolam@gmail.com>
2022-11-29 02:53:57 -0600

    Change default server port to 3000

--- api/utils/constants.js
+++ api/utils/constants.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 if (process.env.NODE_ENV !== "production") dotenv.config();
 
-export const PORT = process.env.PORT || 5000;
+export const PORT = process.env.PORT || 3000;
 export const CONTACT_FK = "contactId";
 
 export const APPLICATION_STATUSES = [
